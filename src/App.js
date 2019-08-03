b123296e6a9dd4ae0b7f538be16d5a7e0ba88427
victor hugo <victor03016@gmail.com>
2019-08-03 11:17:50 +0200

    Consultando la API de Letras

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, Fragment } from "react";
+import axios from 'axios';
 
 //Importamos los componentes
 import Formulario from "./Components/Formulario";
@@ -11,8 +12,16 @@ function App() {
 
 
     //Metoto para consultar la API de las letras de canciones
-    const consultarAPILetra = (busqueda) => {
-      console.log(busqueda);
+    const consultarAPILetra = async (busqueda) => {
+      // hacemos distroccion desde busqueda a los objetos
+      const {artista, cancion} = busqueda;
+
+      const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
+
+      // Consultar la API
+      const resultado = await axios(url);
+     
+      setLetra(resultado.data.lyrics);
     }
 
     return (
