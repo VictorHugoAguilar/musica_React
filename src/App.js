70022030f914cb555203b928650faae3d2ad1dfc
victor hugo <victor03016@gmail.com>
2019-08-03 14:11:18 +0200

    control de errores
    Control de errores cuando no encuentra artista o cancion

--- src/App.js
+++ src/App.js
@@ -11,16 +11,18 @@ function App() {
     const [artista, setArtista] = useState("");
     const [letra, setLetra] = useState([]);
     const [info, setInfo] = useState({});
+    const [error, setError] = useState(false);
 
     //Metoto para consultar la API de las letras de canciones
     const consultarAPILetra = async busqueda => {
         // hacemos distroccion desde busqueda a los objetos
         const { artista, cancion } = busqueda;
 
-        const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
-
+        // console.log("enviar artista de apiletra");
         // Almacenar artista que se almaceno
         setArtista(artista);
+
+        const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
 
         try {
             // Consultar la API
@@ -31,20 +33,37 @@ function App() {
                 setLetra(resultado.data.lyrics);
         } catch {
             setLetra([false]);
-            console.log(letra)
+            // console.log(letra);
         }
     };
 
     // Metodo para consultar la API de informacion del artista
     const consultarAPIInfo = async () => {
+        setError(false);
+
+        // console.log("Entrando en la APIINFO");
+        // console.log(artista);
         if (artista) {
             const url = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
 
+            // console.log("entrando en la consulta de la info");
+
             const resultado = await axios(url);
 
-            setInfo(resultado.data.artists[0]);
-
-            // console.log(info);
+            // console.log(resultado.data.artists[0]);
+            try {
+                if (resultado.data.artists[0].idArtist !== null) {
+                    setInfo(resultado.data.artists[0]);
+                    setError(false);
+                    // console.log(info)
+                } else {
+                    setError(true);
+                    console.log("error en el else" + error);
+                }
+            } catch {
+                setError(true);
+                console.log("error en el cathc" + error);
+            }
         }
     };
 
@@ -60,14 +79,18 @@ function App() {
             <div className="alert alert-danger">No se ha encontrado letra</div>
         );
 
+    const artistaEncontrado = error ? (
+        <div className="alert alert-danger">No se ha artista</div>
+    ) : (
+        <Informacion info={info} />
+    );
+
     return (
         <Fragment>
             <Formulario consultarAPILetra={consultarAPILetra} />
             <div className="container mt-5">
                 <div className="row">
-                    <div className="col-6">
-                        <Informacion info={info} />
-                    </div>
+                    <div className="col-6">{artistaEncontrado}</div>
                     <div className="col-6">{letraEncontrada}</div>
                 </div>
             </div>
